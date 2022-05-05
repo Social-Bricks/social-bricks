039bce29078092b9d25aacdb8ee898759ffc8ed3
Arantor <sleeping@myperch.org>
2022-05-05 22:59:36 +0100

    Add a warning to index.php if people try to run on an old version.

--- index.php
+++ index.php
@@ -32,6 +32,11 @@ define('SB_USER_AGENT', 'Mozilla/5.0 (' . php_uname('s') . ' ' . php_uname('m') 
 if (!defined('TIME_START'))
 	define('TIME_START', microtime(true));
 
+if (version_compare(phpversion(), '8.0.0', '<'))
+{
+	die("PHP 8.0.0 or newer is required, your server has " . phpversion() . ". Please ask your host to upgrade PHP.");
+}
+
 // If anything goes wrong loading Settings.php, make sure the admin knows it.
 error_reporting(E_ALL);
 
@@ -76,8 +81,7 @@ require_once($sourcedir . '/Load.php');
 require_once($sourcedir . '/Security.php');
 
 // Ensure we don't trip over disabled internal functions
-if (version_compare(PHP_VERSION, '8.0.0', '>='))
-	require_once($sourcedir . '/Subs-Compat.php');
+require_once($sourcedir . '/Subs-Compat.php');
 
 // If $maintenance is set specifically to 2, then we're upgrading or something.
 if (!empty($maintenance) &&  2 === $maintenance)
