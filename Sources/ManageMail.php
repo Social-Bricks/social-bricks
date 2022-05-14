849945c40059f705e7f477d78dfd3985446beb62
Arantor <sleeping@myperch.org>
2022-05-14 10:31:01 +0100

    Licence text fix.

--- Sources/ManageMail.php
+++ Sources/ManageMail.php
@@ -11,7 +11,7 @@
  * @package SocialBricks
  * @author Simple Machines https://www.simplemachines.org
  * @copyright 2022 Simple Machines and individual contributors
- * @license https://www.simplemachines.org/about/smf/license.php BSD
+ * @license 3-clause BSD (see accompanying LICENSE file)
  *
  * @version 2.1.0
  */
